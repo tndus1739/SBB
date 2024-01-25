aa93ab85542e5fe744382ae5a7758bfb73cce6bf
Sooyeon Noh <tndus1739@gmail.com>
2024-01-25 18:17:48 +0900

    6시

--- src/main/java/com/mysite/sbb/user/UserController.java
+++ src/main/java/com/mysite/sbb/user/UserController.java
@@ -2,9 +2,11 @@ package com.mysite.sbb.user;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
 @RequiredArgsConstructor
@@ -21,11 +23,31 @@ public class UserController {
 	
 	// 클라이언트가 회원가입 정보를 전송
 	@PostMapping("/user/signup")
-	public String signup (UserCreateForm userCreateForm , Model model) {  // 매게변수 2개
-		System.out.println(userCreateForm.getUsername());
-		System.out.println(userCreateForm.getPassword1());
-		System.out.println(userCreateForm.getPassword2());
-		System.out.println(userCreateForm.getEmail());
+	public String signup (@Valid UserCreateForm userCreateForm , 
+			BindingResult bindingResult,      // 오류가 발생하게 되면 오류정보를 bindingResult에 저장함
+			Model model) {  // 매게변수 2개
+//		System.out.println(userCreateForm.getUsername());
+//		System.out.println(userCreateForm.getPassword1());
+//		System.out.println(userCreateForm.getPassword2());
+//		System.out.println(userCreateForm.getEmail());
+		
+		// 유효성체크에 오류가 발생되었을 때 signup_form.html에 그대로 머물면서 오류 코드를 출력
+		if (bindingResult.hasErrors()) {
+			
+			return "signup_form";
+		}
+		
+		// password1, password2 필드의 값이 같은지 확인 후 같으면
+		if(! userCreateForm.getPassword1().equals(userCreateForm.getPassword2())) {
+			bindingResult.rejectValue("password2","passwordInCorrect", 
+								    	"두개의 패스워드가 일치하지 않습니다");
+			
+			return "signup_form";
+		}
+		
+		
+		// 유효성 검증을 통과하면 DB에 저장
+		userService.create(userCreateForm.getUsername(), userCreateForm.getPassword1(), userCreateForm.getEmail());
 		
 		// 회원가입이 완료되면 "/"로 이동
 		return "redirect:/";
--- src/main/java/com/mysite/sbb/user/UserCreateForm.java
+++ src/main/java/com/mysite/sbb/user/UserCreateForm.java
@@ -1,5 +1,8 @@
 package com.mysite.sbb.user;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,12 +12,18 @@ public class UserCreateForm {
 
 	// client의 값을 받아서 validation하는 객체
 	
+	@Size(min=3, max =25)
+	@NotEmpty(message = "사용자 ID는 필수 입력 사항입니다.")
 	private String username;
 	
+	@NotEmpty(message = "패스워드는 필수 입력 사항입니다.")
 	private String password1;
 
+	@NotEmpty(message = "패스워드는 필수 입력 사항입니다.")
 	private String password2;
 	
+	@NotEmpty(message = "메일주소는 필수 입력 사항입니다.")
+	@Email
 	private String email;
 	
 	
